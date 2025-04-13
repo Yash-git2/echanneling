01ff65a3dd2d90a97fe7788e7d27843d01c4be53
Chalani <chalanifdo721@gmail.com>
2025-04-13 22:40:13 +0530

    urls were added

--- core/urls.py
+++ core/urls.py
@@ -3,4 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('book/', views.book_appointment, name='book_appointment'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('cancel/<int:appointment_id>/', views.cancel_appointment, name='cancel_appointment'),
+
 ]
+
+
