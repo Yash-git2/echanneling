c9c0d2247df4a62795ec6d2392103fa9a989599e
Chalani <chalanifdo721@gmail.com>
2025-04-14 13:22:10 +0530

    New changes updated

--- core/urls.py
+++ core/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('book/', views.book_appointment, name='book_appointment'),
     path('dashboard/', views.dashboard, name='dashboard'),
+    path('cancel/<int:appointment_id>/', views.cancel_appointment, name='cancel_appointment'),
+
 
 ]
