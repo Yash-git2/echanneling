d6daa733c54c45820b1f096f3c8bf5c7553d1b1c
Chalani <chalanifdo721@gmail.com>
2025-04-14 12:57:44 +0530

    Appointment booking view

--- core/views.py
+++ core/views.py
@@ -1,7 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import AppointmentForm
+from .models import Appointment
 
 def home(request):
     return render(request, 'home.html')
 
+def book_appointment(request):
+    if request.method == 'POST':
+        form = AppointmentForm(request.POST)
+        if form.is_valid():
+            appointment = form.save(commit=False)
+            appointment.user = request.user
+            # Check if slot is available
+            exists = Appointment.objects.filter(
+                doctor=appointment.doctor,
+                date=appointment.date,
+                time_slot=appointment.time_slot
+            ).exists()
+            if not exists:
+                appointment.save()
+                return redirect('dashboard')
+            else:
+                form.add_error(None, "This slot is already booked.")
+    else:
+        form = AppointmentForm()
+    return render(request, 'core/book_appointment.html', {'form': form})
 
 # Create your views here.
