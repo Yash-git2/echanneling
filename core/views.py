2dd81d3a18244e1c01519f74cd62e0aca5371a65
Gayasha22 <ridminavodya65@gmail.com>
2025-04-16 14:02:56 +0530

    Added register view with login on success

--- core/views.py
+++ core/views.py
@@ -1,7 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from .forms import RegisterForm  
+
 
 def home(request):
     return render(request, 'home.html')
 
 
-# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('dashboard')  
+    else:
+        form = UserRegisterForm()
+    return render(request, 'register.html', {'form': form})
