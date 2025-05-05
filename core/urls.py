0b82a619aa8185cca469709f5a7fc723013d6a66
Yash-git2 <yashara.t2002@gmail.com>
2025-05-05 14:52:28 +0530

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,12 @@
+#Add URL patterns for appointment booking and lab test booking
+
+
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.home, name='home'), #Displays the homepage
     path('register/', views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
