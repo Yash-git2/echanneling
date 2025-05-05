f9292dc93b5e567678232d5785ff6d3184e369b2
Yash-git2 <yashara.t2002@gmail.com>
2025-05-05 14:53:47 +0530

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('', views.home, name='home'), #Displays the homepage
     path('register/', views.register, name='register'), #Handles new user registration
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'), #Handles user login
-    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'), #Logs out the user
     path('dashboard/', views.dashboard, name='dashboard'),
     path('appointment/', views.view_appointments, name='view_appointments'),
     path('book/', views.book_appointment, name='book_appointment'),
