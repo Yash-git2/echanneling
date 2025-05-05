95f3e05d95e1a4b974d8ef60df904491822211f6
gayasha22 <gayashawebs@gmail.com>
2025-05-05 13:32:50 +0530

    Customize Doctor admin view to show consultation_fee

--- core/admin.py
+++ core/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from .models import Doctor
 from .models import DoctorAvailability
 
+class DoctorAdmin(admin.ModelAdmin):
+    list_display = ('name', 'specialty', 'consultation_fee')  # Add your custom fields here
+
 
 admin.site.register(Doctor)
 admin.site.register(DoctorAvailability)
